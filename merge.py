f2c76fa75e124170703d4653294ae178f324d7ac
haryp2309 <hary2309@gmail.com>
2022-11-11 17:59:40 -0800

    optimized merge script

--- merge.py
+++ merge.py
@@ -7,13 +7,18 @@ import re
 CLIENT_GO = "client.go"
 
 client_path = Path(os.path.dirname(os.path.realpath(__file__)))/"client"
-files = os.listdir(client_path)
+splitted_client_path = client_path.parent/"client_splitted"
+
+os.rename(client_path, splitted_client_path)
+
+files = os.listdir(splitted_client_path)
 files.remove(CLIENT_GO)
 files.insert(0, CLIENT_GO)
 
 mergedContent = ""
 for filename in files:
-    with open(client_path/filename, "r") as f:
+    full_filename = splitted_client_path/filename
+    with open(full_filename, "r") as f:
         content = f.read()
         if filename != CLIENT_GO:
             content = re.sub(r".*import\s*\([^)]*\)(.*)", "", content)
@@ -21,6 +26,8 @@ for filename in files:
             content = content.strip()
             content = f"\n// FILE: {filename.upper()}\n\n"+content
         mergedContent += content
+    os.rename(full_filename, str(full_filename)+".bak")
 
-with open(client_path/"out.go", "w") as f:
+os.mkdir(client_path)
+with open(client_path/CLIENT_GO, "w") as f:
     f.write(mergedContent)
